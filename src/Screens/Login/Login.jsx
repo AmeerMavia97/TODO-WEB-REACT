48451a2e207fbf8d506c52e39aba296f540bc3d3
ameermavia <maviaking97@gmail.com>
2024-01-20 00:14:52 +0500

    login-signup done

--- src/Screens/Login/Login.jsx
+++ src/Screens/Login/Login.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+    <div  className='mt-[60px] ml-[30%] mr-[35%] mb-[100px] border-[1px] border-solid border-black'>
+                <h1 className='font-Lobster mt-[30px] text-[#5f6c76] text-center text-6xl'>Login</h1>
+                <div>
+                    <form >
+                        <input className='w-[80%] h-[45px] mt-10 rounded-md ml-[9%] bg-gray-200 p-3' type="email" placeholder="Enter Your Email"  />
+
+                        <input className='w-[80%] mt-5 h-[45px]  ml-[9%] rounded-md bg-gray-200 p-3' type="Password" placeholder="Enter Your Password" />
+
+                        <input type="password" className='mt-5 w-[80%] h-[45px] rounded-md ml-[9%] p-3 bg-gray-200' placeholder="Enter Your Confirm Password" /> <br />
+
+                        <button className='ml-[45px] px-[173px] mt-5 font-Lobster rounded-md text-lg py-[10px] bg-[#5f6c76] text-white ' type='submit'>Login</button>
+                    </form>
+                    <p className='ml-[120px] mb-12 mt-7 text-sm font-poppins'>Dont have an account?<Link to={'/Register'}>sign up</Link>  </p>
+                </div>
+            </div>
+    </>
   )
 }
 
--- src/Screens/Register/Register.jsx
+++ src/Screens/Register/Register.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <>
+      <div className='mt-[30px] ml-[30%] mr-[30%] mb-[100px] border-[1px] border-solid border-black'>
+        <h1 className='font-Lobster mt-[30px] text-[#5f6c76] text-center text-6xl'>Register </h1>
+        <div>
+          <form>
+            <input className='w-[80%] h-[45px] mt-9 rounded-md ml-[9%] bg-gray-200 p-3' type="text" placeholder="Enter your name" required />
+
+            <input className='w-[80%] h-[45px] mt-5 rounded-md ml-[9%] bg-gray-200 p-3' type="text" placeholder="Enter your NAME" required />
+
+            <input className='w-[80%] h-[45px] mt-5 rounded-md ml-[9%] bg-gray-200 p-3' type="email" placeholder="Enter Your Email" required />
+
+            <input className='w-[80%] h-[45px] mt-5 rounded-md ml-[9%] bg-gray-200 p-3' type="Password" placeholder="Enter your Password" required />
+
+            <input type="password" className='w-[80%] h-[45px] mt-5 rounded-md ml-[9%] bg-gray-200 p-3' placeholder="Enter your confirm Password" required />
+
+            <input type="file" className='ml-[50px]  rounded-md mt-3 bg-white' required /> <br />
+            <button className='ml-[50px] px-[187px] mt-6 font-Lobster rounded-md py-2 bg-[#5f6c76] text-lg text-white ' type='submit'>Register</button>
+          </form>
+          <p className='ml-[140px] mb-10 mt-6 font-poppins text-sm'>Already have an account: <span><Link to={'/'}>Login</Link></span></p>
+        </div>
+      </div>
+    </>
   )
 }
 
